960b6013172c774bf462cb7969087ef22669aa61
green14712-byte <green14712@gmail.com>
2025-10-28 16:12:55 +0900

    1st

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import TopicsList from '@/components/TopicsList'
+import React from 'react'
+
+export default function HomePeage() {
+  return (
+    <div>
+      <h1 className="text-3x1 font-bold">WebDev Topics</h1>
+      <p className="mb-4">MongoDB CRUD example</p>
+      <TopicsList />
+      <TopicsList />
+      <TopicsList />
+    </div>
+  )
+}
